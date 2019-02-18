da7ec50fe28e63c67d917c3b114ea4e34ffb3203
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2019-02-18 13:06:49 -0800

    Updates

---
+++ precache-manifest.ca941d63272d1b7bd486e0e0aef8db90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0a1807e9d7eb2a660c4",
+    "url": "/clickyapp/static/css/main.59ea6edd.chunk.css"
+  },
+  {
+    "revision": "b0a1807e9d7eb2a660c4",
+    "url": "/clickyapp/static/js/main.b0a1807e.chunk.js"
+  },
+  {
+    "revision": "6f999a392cc078ea774a",
+    "url": "/clickyapp/static/js/runtime~main.6f999a39.js"
+  },
+  {
+    "revision": "f39624b01d29e1627497",
+    "url": "/clickyapp/static/js/2.f39624b0.chunk.js"
+  },
+  {
+    "revision": "19d93ae05b22031443bb495bf812b114",
+    "url": "/clickyapp/index.html"
+  }
+];
